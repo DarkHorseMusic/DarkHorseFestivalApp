f691b5d799705a1c3e13b0ba25b379d79328b7a0
Felipe Ceotto <felipe.ceotto@axa-iom.co.im>
2016-11-30 16:37:02 +0000

    Moved mock of schedules to a service that should actually call the back-end (coming soon) and changed the view accordingly.

--- www/js/schedule/schedule.controller.js
+++ www/js/schedule/schedule.controller.js
@@ -2,7 +2,53 @@
     'use strict';
         
     angular.module('darkHorse.controllers')
-    .controller('ScheduleCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
-        
+    .controller('ScheduleCtrl', ['$scope', '$stateParams', '$ionicActionSheet', '$filter', 'ScheduleService', function($scope, $stateParams, $ionicActionSheet, $filter, ScheduleService) {
+        $scope.showScheduleOptions = onShowScheduleOptions;
+        $scope.schedules = {
+            'Friday': null,
+            'Saturday': null,
+            'Sunday': null
+        };
+
+        var allSchedules = null;
+
+        function populateAllSchedules() {
+            ScheduleService.getAllSchedules().then(function(result) {
+                allSchedules = result;
+                $scope.schedules['Friday'] = $filter("filter")(result, { day: 'Friday' }, true);
+                $scope.schedules['Saturday'] = $filter("filter")(result, { day: 'Saturday' }, true);
+                $scope.schedules['Sunday'] = $filter("filter")(result, { day: 'Sunday' }, true);
+            }, function(errorResult) {
+
+            });
+        }
+
+        function onShowScheduleOptions(scheduleId) {
+            var selectedSchedules = $filter("filter")(allSchedules, { id: scheduleId }, true);
+            if ((selectedSchedules === undefined) || (selectedSchedules === null) || (selectedSchedules.length === 0)) {
+                return;
+            }
+
+            var selectedSchedule = selectedSchedules[0];
+
+            $ionicActionSheet.show({
+                buttons: [
+                    {
+                        text: 'Set reminder'
+                    }
+                ],
+                //destructiveText: 'Remove reminder',
+                titleText: selectedSchedule.name,
+                cancelText: 'Cancel',
+                cancel: function() {
+                    // Cancel code here.
+                },
+                buttonClicked: function(index) {
+                    return true;
+                }
+            });
+        };
+
+        populateAllSchedules();
     }]);
 }());
--- www/js/schedule/schedule.service.js
+++ www/js/schedule/schedule.service.js
@@ -2,10 +2,113 @@
     'use strict';
 
     angular.module('darkHorse.services')
-    .factory('ScheduleService', [scheduleService]);
+    .factory('ScheduleService', ['$q', '$ionicAnalytics', 'ANALYTICS_EVENTS', 'API_ENDPOINT', scheduleService]);
     
-    function scheduleService() {
-        var api = {};
+    function scheduleService($q, $ionicAnalytics, ANALYTICS_EVENTS, API_ENDPOINT) {
+        var api = {
+            getAllSchedules: onGetAllSchedules
+        };
+
+        function onGetAllSchedules() {
+            $ionicAnalytics.track(ANALYTICS_EVENTS.GET_ALLSCHEDULES, {});
+
+            return $q(function(resolve, reject) {
+                var schedules = [
+                    {
+                        id: 1,
+                        icon: 'microphone',
+                        name: 'Music Act 1',
+                        day: 'Friday',
+                        startTime: '20:00',
+                        endTime: '21:00',
+                        location: 'Barn',
+                        description: 'Indie rock band that plays really well.'
+                    },
+                    {
+                        id: 2,
+                        icon: 'musical-notes',
+                        name: 'Music Act 2',
+                        day: 'Friday',
+                        startTime: '20:00',
+                        endTime: '21:00',
+                        location: 'Field',
+                        description: 'Indie rock band that plays so so.'
+                    },
+                    {
+                        id: 3,
+                        icon: 'musical-note',
+                        name: 'Music Act 3',
+                        day: 'Friday',
+                        startTime: '20:00',
+                        endTime: '21:00',
+                        location: 'Ditch',
+                        description: 'Indie rock band that really shouldn\'t call itself a band.'
+                    },
+                    {
+                        id: 4,
+                        icon: 'bonfire',
+                        name: 'Folk Act 1',
+                        day: 'Saturday',
+                        startTime: '20:00',
+                        endTime: '21:00',
+                        location: 'Barn',
+                        description: 'Folk duo that plays really well.'
+                    },
+                    {
+                        id: 5,
+                        icon: 'bug',
+                        name: 'Folk Act 2',
+                        day: 'Saturday',
+                        startTime: '20:00',
+                        endTime: '21:00',
+                        location: 'Field',
+                        description: 'Folk trio that plays so so.'
+                    },
+                    {
+                        id: 6,
+                        icon: 'cloudy-night',
+                        name: 'Folk Act 3',
+                        day: 'Saturday',
+                        startTime: '20:00',
+                        endTime: '21:00',
+                        location: 'Ditch',
+                        description: 'A folk. Don\'t really play anything.'
+                    },
+                    {
+                        id: 7,
+                        icon: 'alert',
+                        name: 'Random Act 1',
+                        day: 'Sunday',
+                        startTime: '20:00',
+                        endTime: '21:00',
+                        location: 'Barn',
+                        description: 'We can be stressed about things.'
+                    },
+                    {
+                        id: 8,
+                        icon: 'aperture',
+                        name: 'Random Act 2',
+                        day: 'Sunday',
+                        startTime: '20:00',
+                        endTime: '21:00',
+                        location: 'Field',
+                        description: 'Ran out of icons to add.'
+                    },
+                    {
+                        id: 9,
+                        icon: 'bulb',
+                        name: 'Random Act 3',
+                        day: 'Sunday',
+                        startTime: '20:00',
+                        endTime: '21:00',
+                        location: 'Ditch',
+                        description: 'A really good idea.'
+                    }
+                ];
+
+                resolve(schedules);
+            });
+        };
         
         return api;
     }
